ea13cbc34351bf7e70e2a65d707a7fc2a8d27f0c
finleyhsl23 <finleyhassall@icloud.com>
2026-05-11 19:56:57 +0100

    Delete website/config.js

--- website/config.js
+++
@@ -1,7 +0,0 @@
-// Supabase connection
-// Replace these with the project's public Supabase URL and anon key.
-// The anon key is safe to use in browser code as long as RLS policies are set correctly.
-
-export const SUPABASE_URL = "https://hjdpcfhozhoyeqevnupm.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqZHBjZmhvemhveWVxZXZudXBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5MTk3MzYsImV4cCI6MjA4MjQ5NTczNn0.BXosJO4NmEZOe73GXSGPa3z-i_4ZzF9zBAMBIf6Mkts";
-export const SCHEMA = "thetravellingtavernagreekdeli";
